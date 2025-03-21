fc2a29ea0e7531477615f02803016d14d8dcfde9
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-03-21 17:49:42 +0530

    Configuring Routing in React app

--- react-hooks-frontend/src/App.jsx
+++ react-hooks-frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FooterComponent } from './components/FooterComponent';
 import { HeaderComponent } from './components/HeaderComponent';
-import ListEmployeeComponent from './components/ListEmployeeComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
-
   return (
-      <div>
-        <HeaderComponent></HeaderComponent>
-        <ListEmployeeComponent />
-        <FooterComponent/>
-      </div>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<ListEmployeeComponent />} />
+            <Route exact path="/employees" element={<ListEmployeeComponent />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
+    </div>
   );
 }
 
-export default App
+export default App;
