9212557a7782e56c670b3e57dd8099e63ab010d5
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-03-21 17:50:19 +0530

    Create Employee Rest API

--- CRUD-Operations/src/main/java/com/example/CRUD/Operations/controller/EmployeeController.java
+++ CRUD-Operations/src/main/java/com/example/CRUD/Operations/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.CRUD.Operations.controller;
 import com.example.CRUD.Operations.model.Employee;
 import com.example.CRUD.Operations.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin("*")
@@ -17,5 +14,13 @@ public class EmployeeController {
     private EmployeeRepository repository;
     @GetMapping
     public List<Employee> getAllEmployees()
-    {return repository.findAll();}
+    {return repository.findAll();
+
+    }
+    //build create employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){
+        return repository.save(employee);
+    }
 }
+
